dcd68e2828393f2619cbd51f40659b384d19623f
sobik <sergiy.sobolev@gmail.com>
2015-09-13 22:52:19 +0300

    range slider component + jawr rework

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -4,7 +4,12 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular-route.js"></script>
     <jawr:script src="/script/all.js"/>
+    <link href="<c:url value="/css/bootstrap-combined.min.css"/>" rel="stylesheet">
+    <link href="<c:url value="/css/angular.rangeSlider.css"/>" rel="stylesheet">
     <title>Olimp</title>
 </head>
 <body ng-app="generalModule">
--- src/main/webapp/script/general/generalModule.js
+++ src/main/webapp/script/general/generalModule.js
@@ -1,5 +1,5 @@
 "use strict";
-    var generalModule = angular.module('generalModule', ["ngRoute"]);
+    var generalModule = angular.module('generalModule', ["ngRoute", "ui-rangeSlider"]);
 
     generalModule.config(["$routeProvider", function($routeProvider) {
         $routeProvider
@@ -23,5 +23,80 @@
     var generalController = generalModule.controller("MainCtrl", MainController);
 
     function MainController(){
+
+    }
+
+    generalModule.controller('DemoController', DemoController);
+
+    function DemoController($scope) {
+
+    // just some values for the sliders
+        $scope.demo1 = {
+            min: 20,
+            max: 80
+        };
+
+        $scope.demo2 = {
+            range: {
+                min: 0,
+                max: 10050
+            },
+            minPrice: 1000,
+            maxPrice: 4000
+        };
+
+        $scope.demo3 = {
+            _min: 25,
+            _max: 75,
+            min: function(newValue) {
+                return arguments.length ? ($scope.demo3._min = newValue) : $scope.demo3._min;
+            },
+            max: function(newValue) {
+                return arguments.length ? ($scope.demo3._max = newValue) : $scope.demo3._max;
+            }
+        };
+
+        $scope.demo4 = {
+            rangeMin: 10,
+            rangeMax: 1500,
+            min: 80,
+            max: 1000,
+            disabled: false
+        };
+
+        // function for the button to disable / enable slider 3
+        $scope.toggleDemo4 = function () {
+            // toggle true / false
+            $scope.demo4.disabled = !$scope.demo4.disabled;
+        };
+
+        $scope.demo5 = {
+            min: 300,
+            max: 800
+        };
+
+        $scope.demo6 = {
+            min: 2000,
+            max: 8000
+        };
+
+        $scope.demo7 = {
+            valueA: 5000,
+            valueB: 3000
+        };
+
+        $scope.demo8 = {
+            range: {
+                min: 0,
+                max: 10050
+            },
+            minPrice: 1000,
+            maxPrice: 4000
+        };
+
+        $scope.demo9 = {
+            min: 3000,
+            max: 5000
+        };
 
     }
